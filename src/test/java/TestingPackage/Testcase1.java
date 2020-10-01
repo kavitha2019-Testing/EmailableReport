e004a949df6439c16c8b10b3d1ef0840a6ed98a4
kavitha <kavitha.abburi@gmail.com>
2020-10-01 22:51:29 +0100

    Firstcommit

---
+++ src/test/java/TestingPackage/Testcase1.java
@@ -0,0 +1,26 @@
+package TestingPackage;
+
+import StratPackage.Stratclass;
+import org.testng.annotations.Test;
+
+public class Testcase1  extends Stratclass {
+     @Test
+    public void Reportstest1(){
+         System.out.println("this is reportstest1");
+     }
+
+    @Test
+    public void Reportstest2(){
+        System.out.println("this is reportstest2");
+    }
+    @Test
+    public void Reportstest3(){
+        System.out.println("this is reportstest3");
+    }
+    @Test
+    public void Reportstest4(){
+        System.out.println("this is reportstest4");
+    }
+
+
+}
