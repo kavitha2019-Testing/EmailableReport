288cd366790b630c40b38b96d28e77b9e97eedbb
kavitha <kavitha.abburi@gmail.com>
2020-10-01 23:07:37 +0100

    updatedcommit

--- src/main/java/StratPackage/Stratclass.java
+++ src/main/java/StratPackage/Stratclass.java
@@ -20,7 +20,7 @@ public class Stratclass {
     // opening browser
     @BeforeMethod
     public void browseropen() {
-       if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
+       // if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
             options.setPageLoadStrategy(PageLoadStrategy.NONE);
@@ -31,36 +31,36 @@ public class Stratclass {
             options.addArguments("--disable-browser-side-navigation");
             options.addArguments("--disable-gpu");
             driver = new ChromeDriver(options);
-        } else if (System.getProperty("browser").equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            FirefoxOptions options = new FirefoxOptions();
-            options.setPageLoadStrategy(PageLoadStrategy.NONE);
-            options.addArguments("enable-automation");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-infobars");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-browser-side-navigation");
-            options.addArguments("--disable-gpu");
-            driver = new FirefoxDriver(options);
-        } else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
-            WebDriverManager.iedriver().setup();
-            InternetExplorerOptions options = new InternetExplorerOptions();
-            options.setPageLoadStrategy(PageLoadStrategy.NONE);
-//                                  options.addArguments("enable-automation");
+//        } else if (System.getProperty("browser").equalsIgnoreCase("firefox")) {
+//            WebDriverManager.firefoxdriver().setup();
+//            FirefoxOptions options = new FirefoxOptions();
+//            options.setPageLoadStrategy(PageLoadStrategy.NONE);
+//            options.addArguments("enable-automation");
+//            options.addArguments("--no-sandbox");
+//            options.addArguments("--disable-infobars");
+//            options.addArguments("--disable-dev-shm-usage");
+//            options.addArguments("--disable-browser-side-navigation");
+//            options.addArguments("--disable-gpu");
+//            driver = new FirefoxDriver(options);
+//        } else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
+//            WebDriverManager.iedriver().setup();
+//            InternetExplorerOptions options = new InternetExplorerOptions();
+//            options.setPageLoadStrategy(PageLoadStrategy.NONE);
+////                                  options.addArguments("enable-automation");
 //                                  options.addArguments("--no-sandbox");
 //                                   options.addArguments("--disable-infobars");
 //                                   options.addArguments("--disable-dev-shm-usage");
 //                                   options.addArguments("--disable-browser-side-navigation");
-  //                                 options.addArguments("--disable-gpu");
-            driver = new InternetExplorerDriver(options);
+            //                                 options.addArguments("--disable-gpu");
+            //   driver = new InternetExplorerDriver(options);
+            // }
+            driver.get("https://www.hotukdeals.com/?page=4");
+            driver.manage().window().maximize();
+            driver.manage().deleteAllCookies();
+            driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.MILLISECONDS);
+            driver.manage().timeouts().implicitlyWait(60, TimeUnit.MILLISECONDS);
         }
-        driver.get("https://www.hotukdeals.com/?page=4");
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.MILLISECONDS);
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.MILLISECONDS);
-    }
-    @AfterMethod
+        @AfterMethod
     public void teardown() {
         driver.close();
     }
